c86b05724c43939779257f3a4b47a0fa8ee8641c
Worachat <worachat.smn@gmail.com>
2025-02-01 12:11:21 +0700

    linked backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env(
+    "PUBLIC_URL",
+    "https://deploy-registrar-systems-production.up.railway.app"
+  ),
   app: {
     keys: env.array("APP_KEYS"),
   },
